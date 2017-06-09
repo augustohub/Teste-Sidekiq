f2a70165bc337c618c9cad20f8fa9336fc157736
Augusto Hubert <augusto.hubert@ibccoaching.com.br>
2017-06-09 16:00:45 -0300

    Setting up TwoWorker

--- app/workers/two_worker.rb
+++ app/workers/two_worker.rb
@@ -1,7 +1,9 @@
 class TwoWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform()
+    Import.create(job: 'two')
   end
+
+  Sidekiq::Cron::Job.create(name: 'Two worker - every 2min', cron: '*/2 * * * *', class: 'TwoWorker')
 end
