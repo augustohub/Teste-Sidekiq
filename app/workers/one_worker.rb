eb7c35a3af5570d91246bd8c9d531d4742660336
Augusto Hubert <augusto.hubert@ibccoaching.com.br>
2017-06-09 15:59:42 -0300

    Setting up OneWorker

--- app/workers/one_worker.rb
+++ app/workers/one_worker.rb
@@ -1,7 +1,9 @@
 class OneWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform()
+    Import.create(job: 'one')
   end
+
+  Sidekiq::Cron::Job.create(name: 'One worker - every 1min', cron: '*/1 * * * *', class: 'OneWorker')
 end
