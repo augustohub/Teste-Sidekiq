089d4f5a406ea1e71eca50b0be4f2d627a2e12da
Augusto Hubert <augusto.hubert@ibccoaching.com.br>
2017-06-09 16:30:11 -0300

    Transfering and Setting up sidekiq-cron jobs to schedule.yml

--- app/workers/one_worker.rb
+++ app/workers/one_worker.rb
@@ -4,6 +4,4 @@ class OneWorker
   def perform()
     Import.create(job: 'one')
   end
-
-  Sidekiq::Cron::Job.create(name: 'One worker - every 1min', cron: '*/1 * * * *', class: 'OneWorker')
 end
--- app/workers/two_worker.rb
+++ app/workers/two_worker.rb
@@ -4,6 +4,4 @@ class TwoWorker
   def perform()
     Import.create(job: 'two')
   end
-
-  Sidekiq::Cron::Job.create(name: 'Two worker - every 2min', cron: '*/2 * * * *', class: 'TwoWorker')
 end
